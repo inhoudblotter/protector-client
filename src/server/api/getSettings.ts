85e0216e76f6bbf64d3b55dbbc0b90462f745148
inhoudblotter <inhoudblotter@gmail.com>
2024-03-03 23:57:19 +0500

    change env

--- src/server/api/getSettings.ts
+++ src/server/api/getSettings.ts
@@ -2,7 +2,7 @@ import "dotenv/config";
 import { ISettings } from "src/client/shared/types/ISettings";
 
 export async function getSettings() {
-  const res = await fetch(`${process.env.VITE_API_HOST}/settings`);
+  const res = await fetch(`${process.env.API_HOST}/settings`);
   const data = (await res.json()) as ISettings;
   if (res.ok) return data;
   throw data;
